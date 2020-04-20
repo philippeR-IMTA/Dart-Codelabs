797d6bc68f4406162387555c7591ad1218c25814
Philippe Rateau <philippe.rateau@imt-atlantique.net>
2020-04-20 17:35:28 +0200

    fin du premier codelab

---
+++ First flutter app/startup_namer/lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18); 
+
+  @override
+  Widget build(BuildContext context) {
+    //final WordPair wordPair = WordPair.random();    // N'apparait que dans la partie 4
+    //return Text(wordPair.asPascalCase);             // N'apparait que dans la partie 4
+    return Scaffold (
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );  
+  }
+
+   Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      // The itemBuilder callback is called once per suggested 
+      // word pairing, and places each suggestion into a ListTile
+      // row. For even rows, the function adds a ListTile row for
+      // the word pairing. For odd rows, the function adds a 
+      // Divider widget to visually separate the entries. Note that
+      // the divider may be difficult to see on smaller devices.
+      itemBuilder: (BuildContext _context, int i) {
+        // Add a one-pixel-high divider widget before each row 
+        // in the ListView.
+        if (i.isOdd) {
+          return Divider();
+        }
+
+        // The syntax "i ~/ 2" divides i by 2 and returns an 
+        // integer result.
+        // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
+        // This calculates the actual number of word pairings 
+        // in the ListView,minus the divider widgets.
+        final int index = i ~/ 2;
+        // If you've reached the end of the available word
+        // pairings...
+        if (index >= _suggestions.length) {
+          // ...then generate 10 more and add them to the 
+          // suggestions list.
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      }
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+  
+}
+
+
+/*
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //final WordPair wordPair = WordPair.random();    // N'apparait que dans les partie 3 et 4
+
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: const Center(
+          //child: const Text('Hello World'),         // N'apparait que dans la partie 3
+          //child: Text(wordPair.asPascalCase),       // N'apparait que dans la partie 4
+          child: RandomWords(),
+        ),
+      ),
+    );
+  }
+}
+*/                                                    // Disparait dans la partie 6
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      home: RandomWords(),
+    );
+  }
+}
+//*/                                                    // N'apparait que dans la partie 6
